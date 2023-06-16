0e7c8cfc8007fbb36e567f2bdb70fa4faf12242d
ektagedekar123 <gedekarekta123@gmail.com>
2023-06-16 00:09:43 -0700

    Product Service is created

---
+++ src/main/java/com/lcwd/electronicstore/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.lcwd.electronicstore.services;
+
+import com.lcwd.electronicstore.payloads.PageableResponse;
+import com.lcwd.electronicstore.payloads.ProductDto;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductService {
+
+    ProductDto createProduct(ProductDto productDto);
+
+    ProductDto updateProduct(ProductDto productDto, String productId);
+
+    void deleteProduct(String productId);
+
+    ProductDto getProduct(String productId);
+
+    PageableResponse<ProductDto> getAllProducts(int pageNo, int pageSize, String sortDir, String sortBy);
+
+    PageableResponse<ProductDto> getAllLiveProducts(Pageable pageable, int pageNo, int pageSize, String sortDir, String sortBy);
+
+    PageableResponse<ProductDto> searchByTitle(Pageable pageable, String title);
+}
