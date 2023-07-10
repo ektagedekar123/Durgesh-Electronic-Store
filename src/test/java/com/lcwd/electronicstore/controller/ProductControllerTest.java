116d19ab6a5cd53434f783677cf545ca1df2827e
ektagedekar123 <gedekarekta123@gmail.com>
2023-07-10 16:51:28 -0700

    Test cases are completed for CategorytServiceImplTest.java

---
+++ src/test/java/com/lcwd/electronicstore/controller/ProductControllerTest.java
@@ -0,0 +1,44 @@
+package com.lcwd.electronicstore.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void getSingleProduct() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getAllLiveProducts() {
+    }
+
+    @Test
+    void searchProducts() {
+    }
+
+    @Test
+    void uploadImage() {
+    }
+
+    @Test
+    void serveImage() {
+    }
+}
