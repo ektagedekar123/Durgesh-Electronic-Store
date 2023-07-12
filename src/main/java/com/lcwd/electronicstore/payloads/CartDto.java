a789c973b5bbe8882214a3c462744064ff2f8035
ektagedekar123 <gedekarekta123@gmail.com>
2023-07-11 23:16:31 -0700

    CartItemDto class is created

--- src/main/java/com/lcwd/electronicstore/payloads/CartDto.java
+++ src/main/java/com/lcwd/electronicstore/payloads/CartDto.java
@@ -7,10 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+
 import java.util.ArrayList;
 import java.util.List;
 @AllArgsConstructor
---
+++ src/main/java/com/lcwd/electronicstore/payloads/CartItemDto.java
@@ -0,0 +1,30 @@
+package com.lcwd.electronicstore.payloads;
+
+import com.lcwd.electronicstore.entities.Cart;
+import com.lcwd.electronicstore.entities.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class CartItemDto {
+
+    private Integer cartItemId;
+
+    private Integer quantity;
+
+    private Integer totalPrice;
+
+
+    private ProductDto product;
+
+    private CartDto cart;
+
+}
