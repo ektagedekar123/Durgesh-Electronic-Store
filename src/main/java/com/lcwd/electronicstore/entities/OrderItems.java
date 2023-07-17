8d0631e9f07f5a367e4a5c7a733565cde14b3245
ektagedekar123 <gedekarekta123@gmail.com>
2023-07-17 13:11:40 -0700

    Order Module is Started, Order & OrderItems entities are created

---
+++ src/main/java/com/lcwd/electronicstore/entities/OrderItems.java
@@ -0,0 +1,33 @@
+package com.lcwd.electronicstore.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name= "order_items")
+public class OrderItems {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+     private int orderItemId;
+
+    private int quantity;
+
+    private int totalPrice;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
