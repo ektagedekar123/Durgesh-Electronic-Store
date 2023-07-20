a4fe6b6ffbbc3d205ff20d33773763e3928b9110
ektagedekar123 <gedekarekta123@gmail.com>
2023-07-20 14:51:51 -0700

    OrderService is created

---
+++ src/main/java/com/lcwd/electronicstore/services/OrderService.java
@@ -0,0 +1,20 @@
+package com.lcwd.electronicstore.services;
+
+import com.lcwd.electronicstore.entities.Order;
+import com.lcwd.electronicstore.payloads.OrderDto;
+
+import java.util.List;
+
+public interface OrderService {
+
+    //create Order
+    OrderDto createOrder(OrderDto orderDto, String userId, String cartId);
+
+    //remove Order
+    void removeOrder(String userId, String orderId);
+
+    //get Orders of User
+    List<Order> getOrdersOfUser(String userId);
+
+    //get Orders
+}
