ef76d6d7d432f327af7e1064f234a1d3fa84263e
ektagedekar123 <gedekarekta123@gmail.com>
2023-07-20 14:30:19 -0700

    Repositories are created for Order & OrderItems

--- src/main/java/com/lcwd/electronicstore/repositories/OrderService.java
+++ src/main/java/com/lcwd/electronicstore/repositories/OrderService.java
@@ -1,4 +1,12 @@
 package com.lcwd.electronicstore.repositories;
 
-public interface OrderService {
+import com.lcwd.electronicstore.entities.Order;
+import com.lcwd.electronicstore.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderService extends JpaRepository<Order, String> {
+
+    List<Order> findByUser(User user);
 }
