776502e956842035b95ad631aa3a1a0a06ed58c8
ektagedekar123 <gedekarekta123@gmail.com>
2023-07-21 00:38:42 -0700

    OrderController is Completed

--- src/main/java/com/lcwd/electronicstore/payloads/CreateOrderRequest.java
+++ src/main/java/com/lcwd/electronicstore/payloads/CreateOrderRequest.java
@@ -2,6 +2,8 @@ package com.lcwd.electronicstore.payloads;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,18 +15,23 @@ import java.util.List;
 @Builder
 public class CreateOrderRequest {
 
+    @NotBlank(message= "User Id is required!!")
     private String userId;
 
+    @NotBlank(message= "Cart id is required!!")
     private String cartId;
 
     private String orderStatus="PENDING";
 
     private String paymentStatus="NOTPAID";
 
+    @NotBlank(message= "Address is required!!")
     private String billingAddress;
 
+    @NotEmpty
     private String billingPhone;
 
+    @NotBlank
     private String billingName;
 
 
